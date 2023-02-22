bd4b87a4cd069a8577b2868216e0ab59c50e8550
Wazzupek01 <wazzupekgaming@gmail.com>
2023-02-22 13:52:27 +0100

    added mappers, and dtos, css-modules in frontend and minor changes in code

---
+++ Backend/tobebought/target/generated-sources/annotations/com/pedrycz/tobebought/model/shoppingList/ShoppingListDTOMapperImpl.java
@@ -0,0 +1,11 @@
+package com.pedrycz.tobebought.model.shoppingList;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-01-17T11:19:20+0100",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.1 (N/A)"
+)
+public class ShoppingListDTOMapperImpl implements ShoppingListDTOMapper {
+}
